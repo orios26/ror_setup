980c6dcfa7966e41126f1f42e573eb1de8d4aa7f
orios26 <37675546+orios26@users.noreply.github.com>
2019-01-18 18:22:50 -0600

    Update ror_setup_deb.sh
    added port number 
    fixed incorrect source path 
    added -y option to prevent prompt for gcc g++ make

--- ror_setup_deb.sh
+++ ror_setup_deb.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 #script to install Ruby, Rails, and NodeJS on deb distros 
-gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+gpg --keyserver hkp://pool.sks-keyservers.net:80 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 
 apt install curl -y
 curl -sSL https://get.rvm.io | bash -s stable --ruby
-source /home/$USERNAME/.rvm.scripts/rvm
+source /home/$USERNAME/.rvm/scripts/rvm
 rvm get stable --autolibs=enable
 rvm --default use ruby-2.6.0
 
 curl -sL https://deb.nodesource.com/setup_10.x | bash -
 
 apt install -y nodejs
-apt install gcc g++ make
+apt install gcc g++ make -y
 
 gem update --system
 gem install rails -v 5.2.2
