127a2e283db71ad66094758f544a9be2eaeb7671
jr.rios26@gmail.com <jr.rios26@gmail.com>
2019-02-09 22:27:02 +0000

    made changes to debian ror script

--- ror_setup_deb.sh
+++ ror_setup_deb.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 #script to install Ruby, Rails, and NodeJS on deb distros 
-gpg --keyserver hkp://pool.sks-keyservers.net:80 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 
-apt install curl -y
+sudo apt install curl -y
+command curl -sSL https://rvm.io/pkuczynski.asc | gpg --import -
 curl -sSL https://get.rvm.io | bash -s stable --ruby
-source /home/$USERNAME/.rvm/scripts/rvm
-rvm get stable --autolibs=enable
-rvm --default use ruby-2.6.0
+source /home/$USER/.rvm/scripts/rvm
+rvm install 2.5.1
+rvm --default use ruby-2.5.1
 
-curl -sL https://deb.nodesource.com/setup_10.x | bash -
+curl -sL https://deb.nodesource.com/setup_10.x -o nodesource_setup.sh
+sudo bash nodesource_setup.sh
 
-apt install -y nodejs
-apt install gcc g++ make -y
+sudo apt install -y nodejs
+sudo apt install gcc g++ make -y
 
 gem update --system
-gem install rails -v 5.2.2
+gem install rails -v 5.2.1
 
+echo "*************CHECKING VERSIONS********************"
+echo "RAILS"
+rails --version
+echo "RUBY"
+ruby --version
+echo "NODEJS"
+nodejs --version
 
