bbefae693da2d5ea64a5ca461db11221396a4b1c
orios26 <37675546+orios26@users.noreply.github.com>
2019-03-22 20:08:58 -0500

    Create ror_buntu.sh
    making a script to run on ubuntu

---
+++ ror_buntu.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#script to install Ruby, Rails, and NodeJS on deb distros 
+
+sudo apt install curl -y
+command curl -sSL https://rvm.io/pkuczynski.asc | gpg --import -
+curl -sSL https://get.rvm.io | bash 
+source /home/$USER/.rvm/scripts/rvm
+rvm install 2.5.1
+rvm --default use ruby-2.5.1
+
+curl -sL https://deb.nodesource.com/setup_10.x | sudo bash -
+
+sudo apt install -y nodejs
+sudo apt install gcc g++ make -y
+
+gem update --system
+gem install rails -v 5.2.1
+
+sudo apt install postgresql postgresql-contrib -y
+sudo apt-get install libpq-dev -y
+
+
+echo "*************CHECKING VERSIONS********************"
+echo "RAILS"
+rails --version
+echo "RUBY"
+ruby --version
+echo "NODEJS"
+nodejs --version
+sudo su - postgres -c "SELECT version();"
